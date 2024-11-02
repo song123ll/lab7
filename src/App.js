0cfd6bbc674871c55d2619a9f5c3ceab9da95228
Song Liu <sn806005@dal.ca>
2024-11-02 09:09:21 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Contact Information</h2>
+      <p>Name: Song Liu</p>
+      <p>Bnumber: B00871624</p>
+      <p>Email: sn806005@dal.ca</p>
+    </div>
+  );
+}
+
+export default App;
